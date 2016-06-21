e7bcd4ec6f4868585a3cb64363b4bb0a6b65d5f2
Javier Lopez <javy7@hotmail.com>
2016-06-21 14:46:54 -0400

    Modified MainActivity Added New File 'TestAddFile.java'

--- app/src/main/java/net/javitek/pushup/MainActivity.java
+++ app/src/main/java/net/javitek/pushup/MainActivity.java
@@ -51,7 +51,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void TestUpload() {
-        
+
         int i = 1+1;
+
+        int ii = 2 + 2;
+
+        int iii = i + ii;
     }
 }
---
+++ app/src/main/java/net/javitek/pushup/TestAddFile.java
@@ -0,0 +1,7 @@
+package net.javitek.pushup;
+
+/**
+ * Created by Javier on 6/21/2016.
+ */
+public class TestAddFile {
+}
